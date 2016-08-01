949564a73c2ee5d0e92dbf952cd25d5795ce152e
Larissa Bentes <lgpbentes@gmail.com>
2016-08-01 18:20:27 -0400

    Commit Inicial

---
+++ include/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '36810869');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gcm_chat');
+
+define("GOOGLE_API_KEY", "AIzaSyAhxGdb648GHAGoo1RKMQgPLrb10tn1dGY");
+
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+
+?>
